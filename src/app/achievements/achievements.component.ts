4a968b29f8963f3a9e78cf0a15173ba041af8bca
frtlec <zafer.krk@hotmail.com>
2019-04-04 22:40:10 +0300

    arccss

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -12,13 +12,28 @@ export class AchievementsComponent implements OnInit {
 
   constructor(private achievementsService:AchievementsService) { }
 
-  items:Archievements[];
+  items:Archievements[]=[];
+  color=["#ffd400","#00ff08","#00e7ff","#ff0c00","#680be4"];
   
   ngOnInit() {
 
     this.achievementsService.getAchievements().subscribe(data=>{
-      this.items=data;
 
+      console.log(data);
+      for(let item of data){
+       
+        const array: Archievements = {
+          achievement_date:item.achievement_date,
+          achievement_imgurl:item.achievement_imgurl,
+          achievement_place:item.achievement_place.trim(),
+          achievement_prize:item.achievement_prize,
+          achievement_status:item.achievement_status,
+          achievement_tournament:item.achievement_tournament
+        };
+        this.items.push(array);
+      }
+      
+    
     });
   }
 
